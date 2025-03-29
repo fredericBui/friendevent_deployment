df5d0f7bf84ac195d3a8a8b3a0b4c1cbc90544cc
Frédéric Bui <bui.frederic.pro@gmail.com>
2025-03-29 15:15:21 +0100

    :feat: add egress and install docker

--- README.md
+++ README.md
@@ -27,7 +27,7 @@ terraform apply
 Try to connect to your machine
 ```
 chmod 400 "my-key"
-ssh -i "my-key" <public_dns>
+ssh -i "my-key" <user>@<public_dns>
 ```
 
 # Configure machine with Ansible
--- Terraform/main.tf
+++ Terraform/main.tf
@@ -7,15 +7,23 @@ resource "aws_key_pair" "deployer" {
 # Creating a security group to restrict/allow inbound connectivity
 resource "aws_security_group" "network-security-group" {
   name        = "my-security-group"
-  description = "Allow TLS inbound traffic"
+  description = "Allow TLS inbound & oubound traffic"
 
   ingress {
     description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] 
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # Not recommended to add "0.0.0.0/0" instead we need to be more specific with the IP ranges to allow connectivity from.
   tags = {
     Name = "my-security-group"
@@ -24,9 +32,9 @@ resource "aws_security_group" "network-security-group" {
 
 # Creating Ubuntu EC2 instance
 resource "aws_instance" "ubuntu-vm-instance" {
-  ami             = "ami-084568db4383264d4"
-  instance_type   = "t2.micro"
-  key_name        = aws_key_pair.deployer.key_name
+  ami                    = "ami-084568db4383264d4"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.network-security-group.id]
   tags = {
     Name = "ubuntu-vm"
@@ -38,6 +46,6 @@ output "public_dns" {
 }
 
 resource "local_file" "public_dns" {
-    content  = aws_instance.ubuntu-vm-instance.public_dns
-    filename = "public_dns.txt"
+  content  = aws_instance.ubuntu-vm-instance.public_dns
+  filename = "public_dns.txt"
 }

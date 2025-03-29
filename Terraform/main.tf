fb614d449fa0983324db094801b995750eeb8d2c
Frédéric Bui <bui.frederic.pro@gmail.com>
2025-03-29 16:04:47 +0100

    :feat: add jenkins container and inbound traffic to security group

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -17,6 +17,22 @@ resource "aws_security_group" "network-security-group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "HTTP"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 50000
+    to_port     = 50000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
